83f3e24a4ae92a18c3743e5175af6705966b92cf
MakotoAtsu <chris03152008@gmail.com>
2022-05-04 22:50:20 +0800

    Add LoadingIcon componment

---
+++ lib/widget/Compoments/LoadingIcon.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoadingIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: CircularProgressIndicator());
+}
